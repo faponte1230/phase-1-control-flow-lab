9f0305e8ad00354def477f402ee8b41725f47dbd
Fernando Aponte <faponte1230@yahoo.com>
2022-11-01 19:44:35 -0400

    commit tree ef88a4ab76f426cc49551a629cb8189d889fa8ec

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let freebie = 'This one is on me!'
+  let chargeTwenty = 'That will be twenty bucks.'
+  let chargeThirty = 'I will gladly take your thirty bucks.'
+  let tooFar = 'No can do.'
+  if (distance <= 400){
+    return freebie
+  } else if (distance <= 2000){
+    return chargeTwenty
+  } else if (distance <= 2500){
+    return chargeThirty
+  } else if (distance >=2501){
+    return tooFar
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+const taxiRange = city === 'NYC'? "Ok, sounds good." : "No go."
+return taxiRange
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(string){ 
+ if (string === 'generous'){
+  return "Thank you so much."
+ }
+ if (string === 'not as generous'){
+  return "Thank you."
+ }
+ if  (string === 'thanks for everything'){
+  return "Bye."
+ }
+ }
+ 
